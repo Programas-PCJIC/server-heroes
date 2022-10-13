5254526531d498e20ac9ee6b889e61f2312fd69f
John Florez <john.florez25@gmail.com>
2022-10-13 09:42:51 -0500

    se agrego endpoint formato json

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
     res.send("Server Héroes Versión 1.0.0");
 });
 
+app.get("/poli", (req, res) => {
+    res.json({ hola: "Hola poli 2022"})
+})
+
 app.listen(8088, ()=>{
     console.log("El servidor está activo por el puerto 8088")
 })
